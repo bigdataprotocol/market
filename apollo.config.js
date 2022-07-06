7f8a3193e3c8d889614ef1515241b91830ffe6a2
Vegtam <v3gtam@protonmail.com>
2022-07-06 00:50:40 -0700

    URI changes, not working

--- apollo.config.js
+++ apollo.config.js
@@ -2,7 +2,7 @@ module.exports = {
   client: {
     service: {
       name: 'ocean',
-      url: 'https://query.bigdataprotocolmarket.com/subgraphs/name/oceanprotocol/ocean-subgraph',
+      url: 'https://rinkeby.subgraph.v3.dev.bigdataprotocolmarket.com/subgraphs/name/oceanprotocol/ocean-subgraph',
       // optional disable SSL validation check
       skipSSLValidation: true
     }
