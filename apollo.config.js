a7797b732d8228490f302705f806fd6d2c5c4362
Arsenii Petrovich <arseniy.altoros@gmail.com>
2022-07-17 22:17:16 +0300

    fix config

--- apollo.config.js
+++ apollo.config.js
@@ -2,7 +2,7 @@ module.exports = {
   client: {
     service: {
       name: 'ocean',
-      url: 'https://rinkeby.subgraph.v3.dev.bigdataprotocolmarket.com/subgraphs/name/bigdataprotocol/bdp-subgraph/graphql',
+      url: 'https://rinkeby.subgraph.v3.dev.bigdataprotocolmarket.com/subgraphs/name/bigdataprotocol/bdp-subgraph',
 
       // optional disable SSL validation check
       skipSSLValidation: true
