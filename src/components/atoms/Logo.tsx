a30cf7dfdf4f78b067fc0c17b84e4ba41da1954b
Vegtam <v3gtam@protonmail.com>
2022-09-21 22:59:42 -0700

    new art

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from 'react'
 import { ReactComponent as LogoAssetFull } from '@oceanprotocol/art/logo/logo.svg'
-// import { ReactComponent as LogoAsset } from '../../images/logo.svg'
+import { ReactComponent as LogoAsset } from '../../images/logo.svg'
 import styles from './Logo.module.css'
 
 export default function Logo({
@@ -9,7 +9,7 @@ export default function Logo({
   noWordmark?: boolean
 }): ReactElement {
   return noWordmark ? (
-    <LogoAssetFull className={styles.logo} />
+    <LogoAsset className={styles.logo} />
   ) : (
     <LogoAssetFull className={styles.logo} />
   )
